b9e376a24bb76ee3529c3c9d08bab54e4ff4b93c
amandamb <manda.m.bell@gmail.com>
2013-02-11 13:19:33 -0500

    added user login functionality

--- samlite/urls.py
+++ samlite/urls.py
@@ -4,5 +4,7 @@ from samlite import views
 
 urlpatterns = patterns("",
     url(r"^$", views.index, name="index"),
-    url(r"^save_frame$", views.save_frame, name="save_frame")
+    url(r"^save_frame$", views.save_frame, name="save_frame"),
+    url(r"^login/$", views.login_user, name="login_user"),
+    url(r"^logout/", views.logout_user, name="logout_user")
 )
--- samlite/views.py
+++ samlite/views.py
@@ -1,6 +1,8 @@
 from django.template import RequestContext, loader
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.contrib.auth import authenticate, login, logout
 
 import random
 
@@ -10,6 +12,9 @@ from samlite.models import Sam_frame
 
 import pprint
 
+from django.contrib.auth.decorators import login_required
+
+#@login_required
 def index(request):
     t = loader.get_template("samlite.html")
     c = RequestContext(request, {})
@@ -22,3 +27,27 @@ def save_frame(request):
     sam_frame.set_image_string(image_string)
     sam_frame.save(image_directory)
     return HttpResponse('{"success": true, "id": %s}' % (sam_frame.id))
+
+#process user login
+def login_user(request):
+    state = "Please log in below..."
+    username = password = ''
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                state = "You're successfully logged in!"
+                return HttpResponseRedirect('/samlite')
+            else:
+                state = "Your account is not active, please contact the site admin."
+    return HttpResponseRedirect('/samlite')
+
+    #return render_to_response('samlite.html', {'state':state, 'username': username})
+
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect('/samlite')
