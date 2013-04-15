9e0c3311bf42e7120d09a8c0bb1f03b39b1b7cf1
Chris Macrander <cmacrander@gmail.com>
2013-04-15 15:45:58 -0400

    proposed backbone for sim

--- simlite/views.py
+++ simlite/views.py
@@ -2,8 +2,15 @@
 from django.http import HttpResponse
 from django.template import Context, loader, RequestContext
 
+
 def index(request):
-   t = loader.get_template("simlite.html")
-   c = RequestContext(request, {})
-   return HttpResponse(t.render(c))
+    t = loader.get_template("simlite.html")
+    c = RequestContext(request, {})
+    return HttpResponse(t.render(c))
 #return HttpResponse("Hello, world.")
+
+
+def sprite(request):
+    t = loader.get_template("sprite.html")
+    c = RequestContext(request, {})
+    return HttpResponse(t.render(c))
